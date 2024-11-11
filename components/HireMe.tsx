47848bd758e66fe7a8c5bf93824f410a210e7055
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-11-11 18:52:51 +0545

    add click it tooltip with flicker animation

--- components/HireMe.tsx
+++ components/HireMe.tsx
@@ -3,11 +3,14 @@
 const HireMe = ({ hidden }: { hidden: string }) => {
   return (
     <button
-      className={`absolute bottom-0 right-0 px-6 py-2 ani-button water rounded-xl m-4 shadow-white text-accent shadow-sm  font-semibold
+      className={` group absolute bottom-0 right-0 px-6 py-2 ani-button water rounded-xl m-4 shadow-white text-accent shadow-sm  font-semibold tranimate 
         ${hidden === 'md' ? 'max-lg:hidden' : 'hidden max-lg:block'}
         `}
     >
       Hire Me
+      <h2 className='group-hover:block hidden absolute text-sm font-normal text-primary -top-7 flicker'>
+        Click it
+      </h2>
     </button>
   );
 };
